8fd9372850d497eab68f02a1fa6a55d3e2f72271
Biruk Gizaw <45443263+bgizaw@users.noreply.github.com>
2024-07-06 18:28:25 -0500

    random

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { Link } from "expo-router"
-import { Text, View, Pressable } from "react-native"
+import { Text, View, Pressable, Image, StyleSheet } from "react-native"
 import database from "./firebase/firestoreInitialize"
 import {
   collection,
 } from "firebase/firestore"
 import Building from "./Classes/Building"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 const buildingNames = [
   "Sontag",
@@ -31,14 +38,19 @@ const TrackBuildingName = (buildingName: string) => {
   return buildingInstance
 }
 
+
+
 const ListofBuildings = buildingNames.map(building => {
   return (
     <Link href={`/${building}`} key={building}>
-      <Pressable onPress={() => TrackBuildingName(building)}></Pressable>
+      <Pressable onPress={() => TrackBuildingName(building)}>
+        <Image source={require('../assets/dormButton.png')}></Image>
+      </Pressable>
       {building}
     </Link>
   )
 })
+
 
 const Homepage = () => {
   return (
